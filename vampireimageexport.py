4e5b9c37b646355e7db873c4622822b3e329ab06
2oog <z00gs@outlook.com>
2022-04-17 09:55:41 +0200

    changed directory

--- vampireimageexport.py
+++ vampireimageexport.py
@@ -7,4 +7,4 @@ j = json.loads(open(path + "items.json", 'r').readlines()[0])
 img = Image.open(path + "items.png")
 
 for jpix in j['textures'][0]['frames']:
-    img.crop( (jpix['frame']['x'], jpix['frame']['y'], jpix['frame']['x'] + jpix['frame']['w'], jpix['frame']['y'] + jpix['frame']['h']) ).save(path + 'export/' + jpix['filename'])
+    img.crop( (jpix['frame']['x'], jpix['frame']['y'], jpix['frame']['x'] + jpix['frame']['w'], jpix['frame']['y'] + jpix['frame']['h']) ).save(path + 'img/' + jpix['filename'])
